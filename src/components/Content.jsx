4fe6846ba831a069623312d382961500dcfb94ea
PatricCormac <socolov1897@gmail.com>
2020-03-26 14:58:07 +0500

    Разбил блоки на компоненты и разметил структуру

---
+++ src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://placehold.it/1000x200" alt=""/>
+      </div>
+      <div>
+        ava + descr
+      </div>
+      <div>
+        my posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li><a href="#s">profile</a></li>
+        <li><a href="#s">messages</a></li>
+        <li><a href="#s">news</a></li>
+        <li><a href="#s">music</a></li>
+        <li><a href="#s">settings</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
