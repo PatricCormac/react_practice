2324623cc97e62ab1e388d4f12d2ea2375908419
PatricCormac <socolov1897@gmail.com>
2020-08-05 04:25:09 +0500

    Updates

---
+++ precache-manifest.dc27bd14d02aec4417e0812fab5b5b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59317b27042d045521a634fc95c49a91",
+    "url": "/react_practice/index.html"
+  },
+  {
+    "revision": "1c914dbab56a8f958b95",
+    "url": "/react_practice/static/css/main.638f6a32.chunk.css"
+  },
+  {
+    "revision": "de80562ef7a5b0d31e37",
+    "url": "/react_practice/static/js/2.7fb75a9f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react_practice/static/js/2.7fb75a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c914dbab56a8f958b95",
+    "url": "/react_practice/static/js/main.ac57fd11.chunk.js"
+  },
+  {
+    "revision": "ea5d536db56f202994b7",
+    "url": "/react_practice/static/js/runtime-main.a374be44.js"
+  }
+]);
