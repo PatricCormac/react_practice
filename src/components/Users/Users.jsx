ce6c4c1d81911f22baec980e9d12a653fa9f9188
PatricCormac <socolov1897@gmail.com>
2020-08-05 04:49:06 +0500

    Запрос к API при монтировании компоненты Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,17 +3,13 @@ import styles from "./Users.module.css";
 import * as axios from "axios";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
         this.props.setUsers(response.data.items);
       });
   }
-
-  getUsers = () => {};
 
   render() {
     return (
