13e792c249dd36e72629fe5fd3cf042c743af8b1
PatricCormac <socolov1897@gmail.com>
2020-08-05 04:17:56 +0500

    Переписал Users в классовую компоненту

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,67 @@
-import React from 'react';
-import styles from './Users.module.css';
-import * as axios from 'axios';
+import React from "react";
+import styles from "./Users.module.css";
+import * as axios from "axios";
 
-let Users = (props) => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-          props.setUsers(response.data.items);
-        }
-      );
-    }
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
   }
 
-  return <div className={styles.users}>
-    <button onClick={getUsers}>Get Users</button>
-    {
-      props.users.map(u => <div key={u.id} className={styles.userItem}>
-        <div className={styles.avatarBlock}>
-          <img src={ u.photos.small != null ? u.photos.small : "https://placehold.it/60x60"} className={styles.avatar} />
-          { u.followed
-            ? <button onClick={() => {props.unfollow(u.id)}}>unfollow</button>
-            : <button onClick={() => {props.follow(u.id)}}>follow</button>}
-        </div>
-        <div className={styles.userInfo}>
-          <div>
-            <p>{u.name}</p>
-            <p>{u.status}</p>
+  getUsers = () => {};
+
+  render() {
+    return (
+      <div className={styles.users}>
+        {this.props.users.map((u) => (
+          <div key={u.id} className={styles.userItem}>
+            <div className={styles.avatarBlock}>
+              <img
+                src={
+                  u.photos.small != null
+                    ? u.photos.small
+                    : "https://placehold.it/60x60"
+                }
+                className={styles.avatar}
+              />
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    this.props.unfollow(u.id);
+                  }}
+                >
+                  unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    this.props.follow(u.id);
+                  }}
+                >
+                  follow
+                </button>
+              )}
+            </div>
+            <div className={styles.userInfo}>
+              <div>
+                <p>{u.name}</p>
+                <p>{u.status}</p>
+              </div>
+              <div className={styles.location}>
+                <p>{"u.location.country"}</p>
+                <p>{"u.location.city"}</p>
+              </div>
+            </div>
           </div>
-          <div className={styles.location}>
-            <p>{"u.location.country"}</p>
-            <p>{"u.location.city"}</p>
-          </div>
-        </div>
-      </div>)
-    }
-  </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Users from './Users';
-import { followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
+import React from "react";
+import { connect } from "react-redux";
+import Users from "./Users";
+import { followAC, unfollowAC, setUsersAC } from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users
-  }
-}
+    users: state.usersPage.users,
+  };
+};
 let mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {
@@ -18,8 +18,8 @@ let mapDispatchToProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
-    }
-  }
-}
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
