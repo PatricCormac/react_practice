8518a5e4ff761b398031d74252707e338df37f40
PatricCormac <socolov1897@gmail.com>
2020-08-05 11:33:14 +0500

    Проблема с получением пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,120 @@
 import React from "react";
 import styles from "./Users.module.css";
-import * as axios from "axios";
 
 class Users extends React.Component {
   componentDidMount() {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-      });
+    this.props.setUsers([
+      {
+        name: "VladPatsiuk",
+        id: 9710,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "Ilia_ov",
+        id: 9709,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "Rovel8",
+        id: 9708,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "samo_dur_andre",
+        id: 9707,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "Natalka77712",
+        id: 9706,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "bakhtiyarrem",
+        id: 9705,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "Melon",
+        id: 9704,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "moiz23",
+        id: 9703,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "moiz",
+        id: 9702,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "is_bubux_c",
+        id: 9701,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+    ]);
   }
 
   render() {
